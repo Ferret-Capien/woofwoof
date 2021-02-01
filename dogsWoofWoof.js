c05743bb073e317ae5116588c9a8592e689ff07f
Ferret-Capien <77329402+Ferret-Capien@users.noreply.github.com>
2021-02-01 15:20:42 +0100

    Update dogsWoofWoof.js

--- dogsWoofWoof.js
+++ dogsWoofWoof.js
@@ -13,8 +13,8 @@ bark("spike", 53);
 bark("lady", 17);
 
 bark("juno", 20);
-bark("scottie", -1);
-bark("dino", 0, 0);
-bark("fido", "20");
+bark("scottie", -1);  //Function will simply say woof woof as -1 is less than 20, even though a negative weight for a dog would be odd.
+bark("dino", 0, 0); //Function will ignore second zero...Let's ignore the fact that a dog weighing zero would also be odd...
+bark("fido", "20"); //Function would say woof woof, as it will compare string "20" to 20...
 bark("lady", 10);
 bark("bruno", 21);
