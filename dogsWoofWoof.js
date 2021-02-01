8e3d470584e9c7ef8510f015253d82165b78c22f
Ferret-Capien <77329402+Ferret-Capien@users.noreply.github.com>
2021-02-01 15:16:49 +0100

    Add files via upload

---
+++ dogsWoofWoof.js
@@ -0,0 +1,13 @@
+//CHAPTER 3 - PAGE 80 - WOOF WOOF CODE ANALYSIS
+//BARK FUNCTIONS
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says woof woof");
+    }
+}
+bark("rover", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
